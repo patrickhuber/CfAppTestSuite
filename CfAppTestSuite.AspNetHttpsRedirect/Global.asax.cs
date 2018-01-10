61421976df87a2f3dbf1f00a67c5c8a71260d7f3
Patrick Huber <patrickhuber@hotmail.com>
2018-01-10 11:34:47 -0500

    updates app to exclude /health endpoint from redirection policy.

--- CfAppTestSuite.AspNetHttpsRedirect/Global.asax.cs
+++ CfAppTestSuite.AspNetHttpsRedirect/Global.asax.cs
@@ -23,18 +23,72 @@ namespace CfAppTestSuite.AspNetHttpsRedirect
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
-            EnforceHttps();
-
+            var request = HttpContext.Current.Request;
             var response = HttpContext.Current.Response;
+            var application = HttpContext.Current.ApplicationInstance;
+
+            if (Match(request, "/health"))
+            {
+                Health(request, response);
+            }
+            else
+            {
+                EnforceHttps(request, application);
+                Default(request, response);
+            }
+        }
+
+        private static void Default(HttpRequest request, HttpResponse response)
+        {
             response.Write("Hello World!");
+            Ok(response);
+        }
+
+        private static void Health(HttpRequest request, HttpResponse response)
+        {
+            response.Write("Healthy");
+            Ok(response);
+        }
+
+        private static void Ok(HttpResponse response)
+        {
             response.StatusCode = (int)HttpStatusCode.OK;
             response.End();
         }
 
-        private static void EnforceHttps()
+
+        private static bool Match(HttpRequest request, string route)
         {
-            var context = HttpContext.Current;
-            var request = context.Request;
+            // split the route into segments 
+            var routeSegments = route
+                .Split('/')
+                .Select(x=>x.ToLower())
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToArray();
+
+            // normalize the request path
+            var normalizedSegments = request.Url
+                .Segments
+                .Select(x => x.Trim('/'))
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToArray();
+
+            if (routeSegments.Length > normalizedSegments.Length)
+                return false;
+
+            // just match the route to the request path, ignore any additional path information
+            // in the request uri after the route pattern is done matching
+            for (var i = 0; i < routeSegments.Length; i++)
+            {
+                if (!routeSegments[i].Equals(normalizedSegments[i]))
+                    return false;
+            }
+            return true;
+        }
+
+
+        private static void EnforceHttps(HttpRequest request, HttpApplication application)
+        {
             if (request.IsSecureConnection)
                 return;
 
@@ -43,15 +97,15 @@ namespace CfAppTestSuite.AspNetHttpsRedirect
                 Uri.UriSchemeHttps,
                 StringComparison.OrdinalIgnoreCase))
                 return;
-
+            
             // uncomment for local dev
-            //if (request.IsLocal)
-            //    return;
+            if (request.IsLocal)
+                return;
 
             var response = HttpContext.Current.Response;
             response.RedirectPermanent($"{Uri.UriSchemeHttps}{Uri.SchemeDelimiter}//{request.Url.Host}:443{request.Url.PathAndQuery}");
-            context.ApplicationInstance.CompleteRequest();
-        }
+            application.CompleteRequest();
+        }                
 
         protected void Application_AuthenticateRequest(object sender, EventArgs e)
         {
